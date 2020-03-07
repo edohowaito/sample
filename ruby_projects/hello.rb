16f1790d87b7ffebe814d8e357adfeee3f811b75
”suno” <stargazerastronomy2@gmail.com>
2020-03-07 14:38:13 +0000

    sample2

---
+++ ruby_projects/hello.rb
@@ -0,0 +1,23 @@
+puts "初めてのRuby"
+puts "Ruby"+"はじめました"
+puts 3+3
+puts 3-3
+puts 3*3
+puts 3/3
+
+num_array = [1,2,3,4,5]
+p num_array[0]
+p num_array[1]
+p num_array[2]
+p num_array[3]
+
+p num_array
+
+jibun = {name:'tomoharu',birthday:1994928,blood_type:'O'}
+puts jibun
+
+def plus_ruby(name)
+  "#{name}ruby"
+end
+p plus_ruby("like")
+p plus_ruby("yhee")
